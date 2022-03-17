c5adb4b6b84dbd5561da9313f1459cf6bd5d0577
iambharatmanral <justbharatmanral@gmail.com>
2022-03-17 16:41:41 +0530

    Completed Mad Libs game

--- JavaBasics/TreeStory.java
+++ JavaBasics/TreeStory.java
@@ -3,17 +3,27 @@ import java.io.Console;
 public class TreeStory {
   public static void main(String[] args) {
     Console console = System.console();
+
+    // Tree story template
     // __Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
-    
+
     String ageAString = console.readLine("Enter your age: ");
     int age = Integer.parseInt(ageAString);
     if (age < 13) {
-      console.printf("Sorry you must be at least 13 years old to use this program\n");
+      console.printf("Sorry you must be at least 13 years old to use this program. Exiting...\n");
       System.exit(0);
     }
     String name = console.readLine("Enter your name: ");
     String adjective = console.readLine("Enter an adjective: ");
-    String noun = console.readLine("Enter a noun: ");
+    String noun;
+    boolean isInvalidNoun;
+    do {
+      noun = console.readLine("Enter a noun: ");
+      isInvalidNoun = noun.equalsIgnoreCase("dork") || noun.equalsIgnoreCase("jerk") || noun.equalsIgnoreCase("nerd");
+      if (isInvalidNoun) {
+        console.printf("That language is not allowed. Try again!\n\n");
+      }
+    } while (isInvalidNoun);
     String adverb = console.readLine("Enter an adverb: ");
     String verb = console.readLine("Enter a verb ending in -ing: ");
     console.printf("\nYour TreeStory:\n---------------\n");
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Treehouse Java courses
+Teamtreehouse Java courses
+
+Link to all the courses: https://teamtreehouse.com/library/topic:java
+
+Course Completed: 
+* Java Basics
