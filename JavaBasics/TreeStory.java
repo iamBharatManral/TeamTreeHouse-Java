61deb00e47b31890315278c6cfb8b3aa818f236b
iambharatmanral <justbharatmanral@gmail.com>
2022-03-17 15:43:28 +0530

    Completed first version of Mad libs

--- JavaBasics/TreeStory.java
+++ JavaBasics/TreeStory.java
@@ -3,8 +3,13 @@ import java.io.Console;
 public class TreeStory {
   public static void main(String[] args) {
     Console console = System.console();
+    // __Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
     String name = console.readLine("Enter your name: ");
     String adjective = console.readLine("Enter an adjective: ");
-    console.printf("%s is very %s\n", name, adjective);
+    String noun = console.readLine("Enter a noun: ");
+    String adverb = console.readLine("Enter an adverb: ");
+    String verb = console.readLine("Enter a verb ending in -ing: ");
+    console.printf("\nYour TreeStory:\n---------------\n");
+    console.printf("%s is a %s %s. They are always %s %s.\n", name, adjective, noun, adverb, verb);
   }
 }
