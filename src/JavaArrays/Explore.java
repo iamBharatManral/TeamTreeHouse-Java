d0f115e5b5ae1e851da5188284e84ec51ab65316
iambharatmanral <justbharatmanral@gmail.com>
2022-03-18 09:33:01 +0530

    Arrays with enhanced for loop

---
+++ src/JavaArrays/Explore.java
@@ -0,0 +1,8 @@
+public class Explore {
+  public static void main(String[] args) {
+    String[] friends = { "Ben", "Alena", "Pasan" };
+    for (String friend : friends) {
+      System.out.printf("Hey %s! The movie starts at 7, C U there! \n", friend);
+    }
+  }
+}
