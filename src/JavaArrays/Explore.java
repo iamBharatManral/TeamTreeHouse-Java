3aedba7387a1dd13bafcb6f5982954b3c3f1647c
iambharatmanral <justbharatmanral@gmail.com>
2022-03-18 09:37:02 +0530

    Unenhanced for loop

--- src/JavaArrays/Explore.java
+++ src/JavaArrays/Explore.java
@@ -1,8 +1,17 @@
 public class Explore {
   public static void main(String[] args) {
     String[] friends = { "Ben", "Alena", "Pasan" };
+
+    // Enhanced for loop
     for (String friend : friends) {
       System.out.printf("Hey %s! The movie starts at 7, C U there! \n", friend);
+    }
+    System.out.println("\n");
+
+    // Unenhanced for loop
+    for (int i = 0; i < friends.length; ++i) {
+      String friend = friends[i];
+      System.out.printf("Hola %s, Please be there in time, maybe 10-15 mins before. Thanks\n", friend);
     }
   }
 }
